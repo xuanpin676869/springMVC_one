5db8250059888c3ac371ef87cf18a5ab0e497bfb
dazz148578 <dazz148578@163.com>
2023-07-27 10:29:25 +0800

    ssm整合，基础文件已配

---
+++ src/main/java/com/xuanpin/config/MyBatisConfig.java
@@ -0,0 +1,25 @@
+package com.xuanpin.config;
+
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource){
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setTypeAliasesPackage("com.xuanpin.domain");
+        return factoryBean;
+    }
+
+    @Bean
+    public MapperScannerConfigurer mapperScannerConfigurer(){
+        MapperScannerConfigurer msc = new MapperScannerConfigurer();
+        msc.setBasePackage("com.xuanpin.dao");
+        return msc;
+    }
+}
---
+++ src/main/java/com/xuanpin/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.xuanpin.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan({"com.xuanpin.service"})
+@PropertySource("jdbc.properties")
+@Import({JdbcConfig.class, MyBatisConfig.class})
+public class SpringConfig {
+}
