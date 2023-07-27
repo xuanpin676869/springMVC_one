5dc2416c2ad7bf9f48b2380c9a1a629efd3497b4
dazz148578 <dazz148578@163.com>
2023-07-27 17:24:17 +0800

    表现层数据封装

--- src/main/java/com/xuanpin/controller/BookController.java
+++ src/main/java/com/xuanpin/controller/BookController.java
@@ -16,29 +16,38 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping
-    public boolean save(@RequestBody Book book) {
-        return bookService.save(book);
+    public ReturnFrontendInformation save(@RequestBody Book book) {
+        boolean flag = bookService.save(book);
+        return new ReturnFrontendInformation(flag ? WebServiceStatusCode.SAVE_OK:WebServiceStatusCode.SAVE_ERR,flag);
     }
 
     @PutMapping
-    public boolean update(@RequestBody Book book) {
-        return bookService.update(book);
+    public ReturnFrontendInformation update(@RequestBody Book book) {
+        boolean flag = bookService.update(book);
+        return new ReturnFrontendInformation(flag ? WebServiceStatusCode.UPDATE_OK:WebServiceStatusCode.UPDATE_ERR,flag);
     }
 
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable Integer id) {
-        return bookService.delete(id);
+    public ReturnFrontendInformation delete(@PathVariable Integer id) {
+        boolean flag = bookService.delete(id);
+        return new ReturnFrontendInformation(flag ? WebServiceStatusCode.DELETE_OK:WebServiceStatusCode.DELETE_ERR,flag);
     }
 
     @GetMapping("/{id}")
-    public Book getById(@PathVariable Integer id) {
-        return bookService.getById(id);
+    public ReturnFrontendInformation getById(@PathVariable Integer id) {
+        Book book = bookService.getById(id);
+        Integer code = book != null ? WebServiceStatusCode.GET_OK:WebServiceStatusCode.GET_ERR;
+        String msg = book != null ? "":"数据查询失败，请重试";
+        return new ReturnFrontendInformation(code,book,msg);
     }
 
 
     @GetMapping
-    public List<Book> getAll() {
-        return bookService.getAll();
+    public ReturnFrontendInformation getAll() {
+        List<Book> bookList = bookService.getAll();
+        Integer code = bookList != null ? WebServiceStatusCode.GET_OK:WebServiceStatusCode.GET_ERR;
+        String msg = bookList != null ? "":"数据查询失败，请重试";
+        return new ReturnFrontendInformation(code,bookList,msg);
     }
 
 }
---
+++ src/main/java/com/xuanpin/controller/WebServiceStatusCode.java
@@ -0,0 +1,15 @@
+package com.xuanpin.controller;
+
+public class WebServiceStatusCode {
+    public static final Integer SAVE_OK = 20011;//添加数据成功
+    public static final Integer DELETE_OK = 20021;//删除数据成功
+    public static final Integer UPDATE_OK = 20031;//更新数据成功
+    public static final Integer GET_OK = 20041;//查询数据成功
+
+
+
+    public static final Integer SAVE_ERR = 20010;//添加数据失败
+    public static final Integer DELETE_ERR = 20020;//删除数据失败
+    public static final Integer UPDATE_ERR = 20030;//更新数据失败
+    public static final Integer GET_ERR = 20040;//查询数据失败
+}
