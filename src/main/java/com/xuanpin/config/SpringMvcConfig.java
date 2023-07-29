8f7dea4a4ddcd5c347b66e8833be05f2626867d6
dazz148578 <dazz148578@163.com>
2023-07-29 16:16:25 +0800

    mvc放行webapp

--- src/main/java/com/xuanpin/config/SpringMvcConfig.java
+++ src/main/java/com/xuanpin/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.xuanpin.controller")
+@ComponentScan({"com.xuanpin.controller","com.xuanpin.config"})
 @EnableWebMvc
 public class SpringMvcConfig {
 }
